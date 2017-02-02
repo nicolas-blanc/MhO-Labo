1d3d573396d93cf05559b95c5fff6fed1e6caac1
Nicolas BLANC <nicolas.r.blanc@gmail.com>
2017-02-02 15:12:11 -0500

    Version Recuit done

--- Labo #2/Labo2a Etudiant (Algo descente pour RS)/Recuit.cpp
+++ Labo #2/Labo2a Etudiant (Algo descente pour RS)/Recuit.cpp
@@ -7,6 +7,26 @@
 //arguments du programme dans les propriétés du projet - débogage - arguements.
 //Sinon, utiliser le répertoire execution.
 
+//Permet de calculer le pas d'avancée maximale lors d'une sélection d'une position
+#define FRACTION 3
+// Nombre de tests d'insertion avant la position actuelle
+#define NBTESTSBEFORE 3
+// Nombre de tests d'insertion après la position actuelle
+#define NBTESTSAFTER 3
+
+/* Définition du mode de débug:
+0 : Mode d'affichage minimum avec solution finale
+1 : Mode d'affichage avec affichage des solutions
+2 : Mode d'affichage minimum sans exécution bloquante ni affichage de solution
+*/
+#define DEBUGMODE 0
+
+/* Définition du mode de descente:
+0 : Echange
+1 : Insertion
+*/
+#define CLIMBINGTYPE 1
+
 //*****************************************************************************************
 // Prototype des fonctions se trouvant dans la DLL 
 //*****************************************************************************************
@@ -47,7 +67,13 @@ extern "C" _declspec(dllimport) void	LibererMemoireFinPgm	(TSolution uneCourante
 TSolution GetSolutionVoisine (const TSolution uneSol, TProblem unProb, TRecuit &unRecuit);
 
 //DESCRIPTION:	 Echange de deux villes sélectionnée aléatoirement. NB:uneSol ne doit pas être modifiée
-TSolution	Echange(const TSolution uneSol, TProblem unProb, TRecuit &unRecuit);
+TSolution Echange(const TSolution uneSol, TProblem unProb, TRecuit &unRecuit);
+
+//DESCRIPTION: Insertion d'une commande sélectionnée aléatoirement à différentes positions tirées également aléatoirement
+TSolution Insertion(const TSolution uneSol, const TProblem unProb, TRecuit &unAlgo);
+
+
+void updateTemp(TRecuit LeRecuit) {	LeRecuit.Temperature = LeRecuit.Alpha * LeRecuit.Temperature; }
 
 //******************************************************************************************
 // Fonction main
@@ -76,24 +102,43 @@ int main(int NbParam, char *Param[])
 	CreerSolutionAleatoire(Courante, LeProb, LeRecuit);
 	AfficherSolution(Courante, LeProb, "SolInitiale: ", false);
 
-	LeRecuit.NoPalier = 0;
-	do
+	Best = Courante;
+	LeRecuit.Temperature = LeRecuit.TempInit;
+
+	while (LeRecuit.CptEval < LeRecuit.NB_EVAL_MAX)
 	{
-		LeRecuit.NoPalier ++; //Non utilisé présentement
-		Next = GetSolutionVoisine(Courante, LeProb, LeRecuit);
-		//AfficherSolution(Courante, LeProb, "Courante: ", false);
-		//AfficherSolution(Next, LeProb, "Next: ", false);
-		LeRecuit.Delta = Next.FctObj - Courante.FctObj;
-		if (LeRecuit.Delta <= 0)	//**amélioration
+		LeRecuit.NoPalier = 0;
+		do
 		{
+			Next = GetSolutionVoisine(Courante, LeProb, LeRecuit);
+			//AfficherSolution(Courante, LeProb, "Courante: ", false);
+			//AfficherSolution(Next, LeProb, "Next: ", false);
+			LeRecuit.Delta = Next.FctObj - Courante.FctObj;
+			if (LeRecuit.Delta <= 0)
+			{
 				Courante = Next;
 				cout << "Fct Obj Nouvelle Courante: " << Courante.FctObj << endl;
 				//AfficherSolution(Courante, LeProb, "NouvelleCourante: ", false);
-		}
-	}while (LeRecuit.CptEval < LeRecuit.NB_EVAL_MAX);
+				if (Courante.FctObj < Best.FctObj)
+				{
+					Best = Courante;
+				}
+			}
+			else
+			{
+				if (rand() / double(RAND_MAX) < exp((-LeRecuit.Delta) / LeRecuit.Temperature))
+				{
+					Courante = Next;
+				}
+			}
+			LeRecuit.NoPalier++;
+		} while (LeRecuit.NoPalier != LeRecuit.NbPalier);
 
-	AfficherResultats(Courante, LeProb, LeRecuit);
-	AfficherResultatsFichier(Courante, LeProb, LeRecuit,"Resultats.txt");
+		updateTemp(LeRecuit);
+	}
+
+	AfficherResultats(Best, LeProb, LeRecuit);
+	AfficherResultatsFichier(Best, LeProb, LeRecuit,"Resultats.txt");
 	
 	LibererMemoireFinPgm(Courante, Next, Best, LeProb);
 
@@ -113,12 +158,27 @@ TSolution GetSolutionVoisine(const TSolution uneSol, TProblem unProb, TRecuit &u
 
 	TSolution unVoisin;
 
-	unVoisin = Echange(uneSol, unProb, unRecuit);
+	//Vérification du type de descente choisi
+
+	switch (CLIMBINGTYPE)
+	{
+		//Type Echange
+	case 0:
+		unVoisin = Echange(uneSol, unProb, unRecuit);
+		break;
+		//Type Insertion
+	case 1:
+		unVoisin = Insertion(uneSol, unProb, unRecuit);
+		break;
+	default:
+		//TODO : erreur
+		break;
+	}
 	return (unVoisin);
 }
 
 //DESCRIPTION: Echange de deux commandes sélectionnées aléatoirement
-TSolution Echange(const TSolution uneSol, TProblem unProb, TRecuit &unRecuit)
+TSolution Echange(const TSolution uneSol, TProblem unProb, TRecuit &unAlgo)
 {
 	TSolution Copie;
 	int PosA, PosB, Tmp;
@@ -139,6 +199,123 @@ TSolution Echange(const TSolution uneSol, TProblem unProb, TRecuit &unRecuit)
 	Copie.Seq[PosB] = Tmp;
 
 	//Le nouveau voisin doit être évalué 
-	EvaluerSolution(Copie, unProb, unRecuit);
+	EvaluerSolution(Copie, unProb, unAlgo);
 	return(Copie);
+}
+
+//DESCRIPTION: Création d'une solution temporaire pour l'insertion, décale les éléments selon la position de l'élément à insérer (posA) et sa nouvelle position (pos)
+
+TSolution CreateTempSolution(const TSolution uneSol, int posA, int pos, TProblem unProb)
+{
+	//Création d'une copie de la solution pour réordonner les éléments
+	TSolution copie;
+	CopierSolution(uneSol, copie, unProb);
+
+	for (int i = 0; i < unProb.NbCom; i++)
+	{
+		//Place l'élément posA à sa nouvelle position pos
+		if (i == pos)
+		{
+			copie.Seq[i] = uneSol.Seq[posA];
+		}
+		//Si l'élément i se trouve après pos et avant posA, il faut le décaler vers la position suivante (cas où pos < posA)
+		else if (i > pos && i <= posA)
+		{
+			copie.Seq[i] = uneSol.Seq[i - 1];
+		}
+		//Si l'élément i se trouve avant pos et après posA, il faut le décaler vers la position précédente (cas où posA < pos)
+		else if (i < pos && i >= posA)
+		{
+			copie.Seq[i] = uneSol.Seq[i + 1];
+		}
+		//Sinon, on recopie l'élément
+		else
+		{
+			copie.Seq[i] = uneSol.Seq[i];
+		}
+	}
+
+
+	return copie;
+}
+
+//DESCRIPTION: Comparaison de deux solutions et retourne la meilleure (objectif le plus faible)
+TSolution compare(TSolution tmpSol, TSolution bestTmpSol)
+{
+	if (tmpSol.FctObj < bestTmpSol.FctObj)
+	{
+		return tmpSol;
+	}
+	else
+	{
+		return bestTmpSol;
+	}
+}
+
+//DESCRIPTION: Insertion d'une commande sélectionnée aléatoirement à différentes positions tirées également aléatoirement
+TSolution Insertion(const TSolution uneSol, const TProblem unProb, TRecuit &unAlgo)
+{
+	TSolution bestTmpSol, tmpSol;
+	int posA, posPred;
+	//Création d'un vecteur de positions, qui indique les positions tirées aléatoirement
+	//Ne sert pas ici
+	vector<int> posSol;
+
+	CopierSolution(uneSol, bestTmpSol, unProb);
+
+	//On tire aléatoirement une commande
+	posA = rand() % unProb.NbCom;
+	posPred = posA;
+
+	//On tire également des positions aléatoires d'insertion avant posA à tester
+	//On vérifie que la nouvelle position est correcte, et que le nombre de tests d'insertion est inférieur au maximum
+	int i = 0;
+	while (posPred >= 0 && i < NBTESTSBEFORE)
+	{
+		posPred = posPred - rand() % (unProb.NbCom / FRACTION) - 1;
+
+		if (posPred > 0)
+		{
+			posSol.push_back(posPred);
+		}
+
+		i++;
+	}
+
+	//On tire également des positions aléatoires d'insertion après posA à tester
+	i = 0;
+	posPred = posA;
+	//On vérifie que la nouvelle position est correcte, et que le nombre de tests d'insertion est inférieur au maximum
+	while (posPred < unProb.NbCom && i < NBTESTSAFTER)
+	{
+		posPred = posPred + rand() % (unProb.NbCom / FRACTION) + 1;
+
+		if (posPred < unProb.NbCom)
+		{
+			posSol.push_back(posPred);
+		}
+
+		i++;
+	}
+
+	//On vérifie pour chaque position tirée si la solution créée améliore ou non l'actuelle, et on la conserve si c'est le cas
+	for each (int pos in posSol)
+	{
+		tmpSol = CreateTempSolution(uneSol, posA, pos, unProb);
+
+		if (unAlgo.CptEval < unAlgo.NB_EVAL_MAX)
+		{
+			EvaluerSolution(tmpSol, unProb, unAlgo);
+			//Affichage de la solution intermédiaire proposée
+			if (DEBUGMODE == 1)
+				AfficherSolution(tmpSol, unProb, "----- test intermediaire ----- PosA : " + std::to_string(posA) + " / pos : " + std::to_string(pos), false);
+
+			bestTmpSol = compare(tmpSol, bestTmpSol);
+		}
+	}
+
+	if (DEBUGMODE == 1)
+		AfficherSolution(bestTmpSol, unProb, "===== test final =====", false);
+
+	return bestTmpSol;
 }
